9281a192da3eea3bc6b5d0b00cd9a68a51a2a7d4
Caio Santos <caiosantosfreitas@hotmail.com>
2020-09-28 17:39:30 -0300

    adicionado graficos de idade, bens, genero, escolaridade

--- jsonsave.js
+++ jsonsave.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const fetch = require('node-fetch')
+const cheerio = require('cheerio')
 
 const urlCandidatos = 'http://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/listar/2020/64254/2030402020/13/candidatos'
 
@@ -8,11 +9,16 @@ const urlCandidato = id => {
 } 
 
 const fetchAsync = async url => {
-  const response = await fetch(url);
-  return await response.json();
+  const response = await fetch(url)
+  return await response.json()
 }
 
-const getData = async _ => {
+const fetchAsyncHtml = async url => {
+  const response = await fetch(url)
+  return await response.text()
+}
+
+const getDataTSE = async _ => {
   var jsonData = await fetchAsync(urlCandidatos)
 
   save(jsonData, 'candidatos')
@@ -25,8 +31,75 @@ const getData = async _ => {
   }
 }
 
+const getDataCamara = async _ => {
+  const baseUrl = 'https://franca.sp.leg.br'
+  const arrUrls = [
+    '/pt-br/vereadores/legislaturas/2017-2020',
+    '/pt-br/vereadores/legislaturas/2013-2016',
+    '/pt-br/vereadores/legislaturas/2009-2012',
+    '/pt-br/vereadores/legislaturas/2005-2008',
+    '/pt-br/vereadores/legislaturas/2001-2004',
+    '/pt-br/vereadores/legislaturas/1997-2000',
+    '/pt-br/vereadores/legislaturas/1993-1996',
+    '/pt-br/vereadores/legislaturas/1989-1992',
+    '/pt-br/vereadores/legislaturas/1983-1988',
+    '/pt-br/vereadores/legislaturas/1977-1982'
+  ]
+
+  let atuais = []
+  let antigos = []
+
+  for (url of arrUrls) {
+    var html = await fetchAsyncHtml(baseUrl + url)
+    const selector = cheerio.load(html);
+
+    for (i = 1; i < 8; i++) {
+      for (j = 1; j < 4; j++) {
+        const urlDetalhe = selector(`#block-bartik-content > div > div > div > div.view-content > div > div.views-row.clearfix.row-${i} > div.views-col.col-${j} > article > header > h2 > a`)
+          .attr('href')
+
+        if (urlDetalhe) {
+          var htmlDetalhe = await fetchAsyncHtml(baseUrl + urlDetalhe)
+          const selectorDetalhe = cheerio.load(htmlDetalhe);
+
+          const nomeCompleto = removeAcento(selectorDetalhe('#block-bartik-content > div > div > div.group-left > div.field.field--name-field-nome-completo.field--type-string.field--label-above > div.field__item')
+            .text())
+          
+          if (url == '/pt-br/vereadores/legislaturas/2017-2020') {
+            atuais.push({ nomeCompleto })
+          } else {
+            let atual = false
+            for (vereador of atuais) {
+              if (vereador.nomeCompleto == nomeCompleto) {
+                atual = true
+              }
+            }
+
+            if (!atual) {
+              antigos.push({ nomeCompleto })
+            }
+          }
+        }
+      }
+    }
+  }
+  save(atuais, 'atuais')
+  save(antigos, 'antigos')
+}
+
+function removeAcento(text) {       
+    text = text.toLowerCase();                                                         
+    text = text.replace(new RegExp('[ÁÀÂÃ]','gi'), 'a');
+    text = text.replace(new RegExp('[ÉÈÊ]','gi'), 'e');
+    text = text.replace(new RegExp('[ÍÌÎ]','gi'), 'i');
+    text = text.replace(new RegExp('[ÓÒÔÕ]','gi'), 'o');
+    text = text.replace(new RegExp('[ÚÙÛ]','gi'), 'u');
+    text = text.replace(new RegExp('[Ç]','gi'), 'c');
+    return text;                 
+}
+
 const save = (jsonData, fileName) => {
-  var jsonContent = JSON.stringify(jsonData)
+  const jsonContent = JSON.stringify(jsonData)
   
   fs.writeFile(`json\\${fileName}.json`, jsonContent, 'utf8', function (err) {
       if (err) {
@@ -37,5 +110,6 @@ const save = (jsonData, fileName) => {
       console.log(`${fileName} - JSON file has been saved.`)
   })
 }
- 
-getData()
+
+getDataTSE()
+getDataCamara()
--- script.js
+++ script.js
@@ -3,24 +3,15 @@ const fetchAsync = async url => {
   return await response.json();
 }
 
-const getGraph = (siglas, novatos, jaCandidatos, jaVereadores, atuaisVereadores) => {
-  let options = {
+const getGraphBens = (siglas, mediaPartido) => {
+  const options = {
     series: [{
-    name: 'Novatos',
-    data: novatos
-  }, {
-    name: 'Já foram candidatos mas não se elegeram',
-    data: jaCandidatos
-  }, {
-    name: 'Ex-vereadores',
-    data: jaVereadores
-  }, {
-    name: 'Atuais vereadores',
-    data: atuaisVereadores
+    name: 'Valor',
+    data: mediaPartido
   }],
     chart: {
     type: 'bar',
-    height: 1000,
+    height: 700,
     stacked: true,
   },
   plotOptions: {
@@ -53,7 +44,182 @@ const getGraph = (siglas, novatos, jaCandidatos, jaVereadores, atuaisVereadores)
   }
   }
 
-  let chart = new ApexCharts(document.querySelector("#chart"), options);
+  let chart = new ApexCharts(document.querySelector("#chartBens"), options);
+  chart.render()
+}
+
+const getGraphIdade = (siglas, mediaIdade) => {
+  const options = {
+    series: [{
+    name: 'Idade',
+    data: mediaIdade
+  }],
+    chart: {
+    type: 'bar',
+    height: 700,
+    stacked: true,
+  },
+  plotOptions: {
+    bar: {
+      horizontal: true,
+    },
+  },
+  stroke: {
+    width: 1,
+    colors: ['#fff']
+  },
+  title: {
+    text: ' '
+  },
+  xaxis: {
+    categories: siglas,
+  },
+  yaxis: {
+    title: {
+      text: undefined
+    },
+  },
+  fill: {
+    opacity: 1
+  },
+  legend: {
+    position: 'top',
+    horizontalAlign: 'left',
+    offsetX: 40
+  }
+  }
+
+  let chart = new ApexCharts(document.querySelector("#chartIdade"), options);
+  chart.render()
+}
+
+const getGraphEleicoesAnteriores = (siglas, novatos, jaCandidatos, jaVereadores, atuaisVereadores) => {
+  const options = {
+    series: [{
+    name: 'Novatos',
+    data: novatos
+  }, {
+    name: 'Já foram candidatos mas não se elegeram',
+    data: jaCandidatos
+  }, {
+    name: 'Ex-vereadores',
+    data: jaVereadores
+  }, {
+    name: 'Atuais vereadores',
+    data: atuaisVereadores
+  }],
+    chart: {
+    type: 'bar',
+    height: 700,
+    stacked: true,
+  },
+  plotOptions: {
+    bar: {
+      horizontal: true,
+    },
+  },
+  stroke: {
+    width: 1,
+    colors: ['#fff']
+  },
+  title: {
+    text: ' '
+  },
+  xaxis: {
+    categories: siglas,
+  },
+  yaxis: {
+    title: {
+      text: undefined
+    },
+  },
+  fill: {
+    opacity: 1
+  },
+  legend: {
+    position: 'top',
+    horizontalAlign: 'left',
+    offsetX: 40
+  }
+  }
+
+  let chart = new ApexCharts(document.querySelector("#chartEleicoesAnteriores"), options);
+  chart.render()
+}
+
+const getGraphRaca = (racas, index) => {
+  const options = {
+    series: racas,
+    chart: {
+      width: 380,
+      type: 'pie',
+    },
+    labels: ['Branca', 'Parda', 'Preta', 'Sem Informação'],
+    responsive: [{
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 200
+        },
+        legend: {
+          position: 'bottom'
+        }
+      }
+    }]
+  }
+
+  let chart = new ApexCharts(document.querySelector(`#chartEleicoesRaca${index}`), options);
+  chart.render()
+}
+
+const getGraphGenero = (generos, index) => {
+  const options = {
+    series: generos,
+    chart: {
+      width: 380,
+      type: 'pie',
+    },
+    labels: ['Masculino', 'Feminino'],
+    responsive: [{
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 200
+        },
+        legend: {
+          position: 'bottom'
+        }
+      }
+    }]
+  }
+
+  let chart = new ApexCharts(document.querySelector(`#chartEleicoesGenero${index}`), options);
+  chart.render()
+}
+
+const getGraphEscolaridade = (niveisEscolaridade, index) => {
+  const options = {
+    series: niveisEscolaridade,
+    colors : ['#008FFB', '#00E396', '#FEB019', '#FF4560', '#775DD0', '#5C4742'],
+    chart: {
+      width: 380,
+      type: 'pie',
+    },
+    labels: ['Ensino fundamental incompleto', 'Ensino fundamental completo', 'Ensino medio incompleto', 'Ensino medio completo', 'Superior incompleto', 'Superior completo'],
+    responsive: [{
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 200
+        },
+        legend: {
+          position: 'bottom'
+        }
+      }
+    }]
+  }
+
+  let chart = new ApexCharts(document.querySelector(`#chartEleicoesEscolaridade${index}`), options);
   chart.render()
 }
 
@@ -70,9 +236,13 @@ const getData = async _ => {
     return 0;
   })
 
-  let siglas = [], novatos = [], jaCandidatos = [], jaVereadores = [], atuaisVereadores = []
-  let novatosConta = 0, jaCandidatosConta = 0, jaVereadoresConta = 0, atuaisVereadoresConta = 0
-  let sigla
+  let siglas = [], novatos = [], jaCandidatos = [], jaVereadores = [], atuaisVereadores = [], racas = [0, 0, 0, 0]
+  let niveisEscolaridade = [0, 0, 0, 0, 0, 0], generos = [0, 0], mediaPartido = [], mediaIdade = []
+  let novatosConta = 0, jaCandidatosConta = 0, jaVereadoresConta = 0, atuaisVereadoresConta = 0, candidatosConta = 0
+  let sigla, indexPartido = 0, totalDeBens = 0, totalIdade = 0
+
+  const atuais = await fetchAsync(`json\\atuais.json`)
+  const antigos = await fetchAsync(`json\\antigos.json`)
 
   for (const [index, candidato] of candidatos.entries()) {
     if (0 !== index) {
@@ -82,23 +252,84 @@ const getData = async _ => {
         novatos.push(novatosConta)
         jaCandidatos.push(jaCandidatosConta)
         jaVereadores.push(jaVereadoresConta)
-        novatosConta = 0, jaCandidatosConta = 0, jaVereadoresConta = 0, atuaisVereadoresConta = 0
+        mediaPartido.push((totalDeBens / candidatosConta).toFixed(2))
+        mediaIdade.push((totalIdade / candidatosConta).toFixed(2))
+
+        indexPartido++
+        getGraphRaca(racas, indexPartido)
+        getGraphEscolaridade(niveisEscolaridade, indexPartido)
+        getGraphGenero(generos, indexPartido)
+
+        novatosConta = 0, jaCandidatosConta = 0, jaVereadoresConta = 0, atuaisVereadoresConta = 0, candidatosConta = 0, totalDeBens = 0, totalIdade = 0
+        racas = [0, 0, 0, 0]
+        niveisEscolaridade = [0, 0, 0, 0, 0, 0]
+        generos = [0, 0]
       }
     }
     sigla = candidato.nomeColigacao
+
     novatosConta++
+    candidatosConta++
 
-    let candidatoDetalhes = await fetchAsync(`json\\${(candidato.id)}.json`)
+    const nomeCompleto = removeAcento(candidato.nomeCompleto)
 
-    candidatoDetalhes.eleicoesAnteriores.every(eleicao => {
-      if ((eleicao.nrAno !== 2020) && (eleicao.cargo == 'Vereador')) {
-        jaCandidatosConta++
-        novatosConta--
-        return false
-      } else {
-        return true
+    const candidatoDetalhes = await fetchAsync(`json\\${(candidato.id)}.json`)
+
+    const raca = removeAcento(candidatoDetalhes.descricaoCorRaca)
+    const escolaridade = removeAcento(candidatoDetalhes.grauInstrucao)
+    const genero = removeAcento(candidatoDetalhes.descricaoSexo)
+    totalDeBens += candidatoDetalhes.totalDeBens
+    const data = candidatoDetalhes.dataDeNascimento
+    totalIdade += calculateAge(data.substr(5, 2), data.substr(8, 2), data.substr(0, 4))
+
+    console.log(sigla + ' - ' + nomeCompleto + ' - ' + calculateAge(data.substr(5, 2), data.substr(8, 2), data.substr(0, 4)))
+
+    if (raca == 'branca') racas[0]++
+    if (raca == 'parda') racas[1]++
+    if (raca == 'preta') racas[2]++
+    if (raca == 'sem informacao') racas[3]++
+
+    if (escolaridade == 'ensino fundamental incompleto') niveisEscolaridade[0]++
+    if (escolaridade == 'ensino fundamental completo') niveisEscolaridade[1]++
+    if (escolaridade == 'ensino medio incompleto') niveisEscolaridade[2]++
+    if (escolaridade == 'ensino medio completo') niveisEscolaridade[3]++
+    if (escolaridade == 'superior incompleto') niveisEscolaridade[4]++
+    if (escolaridade == 'superior completo') niveisEscolaridade[5]++
+
+    if (genero == 'masc.') generos[0]++
+    if (genero == 'fem.') generos[1]++
+
+    let atual = false
+    for (vereador of atuais) {
+      if (vereador.nomeCompleto == nomeCompleto) {
+        atual = true
       }
-    })
+    }
+    if (atual) {
+      atuaisVereadoresConta++
+      novatosConta--
+    } else {
+      let antigo = false
+      for (vereador of antigos) {
+        if (vereador.nomeCompleto == nomeCompleto) {
+          antigo = true
+        }
+      }
+      if (antigo) {
+        jaVereadoresConta++
+        novatosConta--
+      } else {
+        candidatoDetalhes.eleicoesAnteriores.every(eleicao => {
+          if ((eleicao.nrAno !== 2020) && (eleicao.cargo == 'Vereador')) {
+            jaCandidatosConta++
+            novatosConta--
+            return false
+          } else {
+            return true
+          }
+        })
+      }
+    }
   }
 
   siglas.push(sigla)
@@ -106,8 +337,44 @@ const getData = async _ => {
   novatos.push(novatosConta)
   jaCandidatos.push(jaCandidatosConta)
   jaVereadores.push(jaVereadoresConta)
+  mediaPartido.push((totalDeBens / candidatosConta).toFixed(2))
+  mediaIdade.push((totalIdade / candidatosConta).toFixed(2))
 
-  getGraph(siglas, novatos, jaCandidatos, jaVereadores, atuaisVereadores)
+  getGraphEleicoesAnteriores(siglas, novatos, jaCandidatos, jaVereadores, atuaisVereadores)
+  getGraphBens(siglas, mediaPartido)
+  getGraphIdade(siglas, mediaIdade)
+
+  indexPartido++
+  getGraphRaca(racas, indexPartido)
+  getGraphEscolaridade(niveisEscolaridade, indexPartido)
+  getGraphGenero(generos, indexPartido)
+}
+
+function removeAcento(text) {       
+  text = text.toLowerCase();                                                         
+  text = text.replace(new RegExp('[ÁÀÂÃ]','gi'), 'a');
+  text = text.replace(new RegExp('[ÉÈÊ]','gi'), 'e');
+  text = text.replace(new RegExp('[ÍÌÎ]','gi'), 'i');
+  text = text.replace(new RegExp('[ÓÒÔÕ]','gi'), 'o');
+  text = text.replace(new RegExp('[ÚÙÛ]','gi'), 'u');
+  text = text.replace(new RegExp('[Ç]','gi'), 'c');
+  return text;                 
+}
+
+function calculateAge(birthMonth, birthDay, birthYear) {
+  var currentDate = new Date();
+  var currentYear = currentDate.getFullYear();
+  var currentMonth = currentDate.getMonth();
+  var currentDay = currentDate.getDate(); 
+  var calculatedAge = currentYear - birthYear;
+
+  if (currentMonth < birthMonth - 1) {
+      calculatedAge--;
+  }
+  if (birthMonth - 1 == currentMonth && currentDay < birthDay) {
+      calculatedAge--;
+  }
+  return calculatedAge;
 }
 
 getData()
